0cbb372b791693aa59c033f65cdca6f62b13c209
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2023-01-06 16:35:40 +0200

    fix

--- src/components/MovieList/MovieList.styled.jsx
+++ src/components/MovieList/MovieList.styled.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const MovieListWraper = styled.ul`
@@ -14,7 +14,7 @@ export const MovieListLine = styled.li`
   padding-bottom: ${p => p.theme.space[2]}px;
 `;
 
-export const MovieListLink = styled(NavLink)`
+export const MovieListLink = styled(Link)`
   color: ${p => p.theme.colors.dark};
   text-decoration: none;
 
